1ec66bbf6b2b24fc09299e23aa1e5b846643b4f8
jean <jeancris95@gmail.com>
2023-08-24 01:24:02 +0200

    primer controlador Restaurant

--- booking-restaurant-api/src/main/java/com/book/bookingrestaurantapi/controllers/RestaurantController.java
+++ booking-restaurant-api/src/main/java/com/book/bookingrestaurantapi/controllers/RestaurantController.java
@@ -1,5 +1,23 @@
 package com.book.bookingrestaurantapi.controllers;
 
-public class RestaurantController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.book.bookingrestaurantapi.exceptions.BookingException;
+import com.book.bookingrestaurantapi.jsons.RestaurantRest;
+import com.book.bookingrestaurantapi.responses.BookingResponse;
+import com.book.bookingrestaurantapi.services.RestaurantService;
+
+@RestController
+@RequestMapping(path = "/booking-restaurant" + "v1")
+public class RestaurantController {
+	@Autowired
+	RestaurantService restaurantService;
+
+	public BookingResponse<RestaurantRest> getRestaurantById(Long restaurantId) throws BookingException {
+		return new BookingResponse<>("Succes", String.valueOf(HttpStatus.OK), "OK",
+				restaurantService.getRestaurantById(restaurantId));
+	}
 }
